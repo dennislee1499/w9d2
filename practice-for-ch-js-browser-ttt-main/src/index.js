5ae72cdd3cdf258e4e5a3ea3349b07be3e684941
Dan Lee <danielhlee4@gmail.com>
2023-08-22 14:37:13 -0400

    set up webpack, add .gitignore

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,4 +1,6 @@
-Add your import statements for View and Game here
+// Add your import statements for View and Game here
+import Game from "../ttt_node/game.js";
+import View from "./ttt-view.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
