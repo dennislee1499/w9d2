61df2f6406b761cbecaa27d398fa0228e28c4786
Dan Lee <danielhlee4@gmail.com>
2023-08-22 17:33:37 -0400

    add css styling

--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -9,7 +9,7 @@ class View {
     let ul = document.createElement("ul");
 
     for (let i = 0; i < 3; i++) {
-      for (let j = 0; j< 3; j++) {
+      for (let j = 0; j < 3; j++) {
         let li = document.createElement("li");
         li.setAttribute("data-row", `${i}`);
         li.setAttribute("data-col", `${j}`);
